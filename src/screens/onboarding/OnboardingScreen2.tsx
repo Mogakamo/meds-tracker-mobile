b4bbe128d58fc8384730cf2584e8b47482e9b8af
Mogakamo <mogaka.amo254@gmail.com>
2022-08-24 17:38:37 +0300

    build onboarding screen 2is done

--- src/screens/onboarding/OnboardingScreen2.tsx
+++ src/screens/onboarding/OnboardingScreen2.tsx
@@ -4,6 +4,7 @@ import tw from '../../lib/tailwind';
 import {useNavigation} from '@react-navigation/native';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import classNames from 'classnames';
+import {SafeAreaView} from 'react-native-safe-area-context';
 
 // type Props = NativeStackScreenProps<>;
 type Props = {
@@ -19,27 +20,42 @@ const OnboardingScreen2 = ({route, navigation}: Props) => {
       headerShown: false,
     });
   }, []);
+
   return (
-    <View style={tw`items-center justify-center h-full mx-3`}>
-      <Image
-        source={{
-          uri: 'https://rebrand.ly/daff5b',
-        }}
-        style={tw`h-56 w-56 bg-transparent border-transparent`}
-      />
-      <Text style={tw`text-3xl font-bold text-black pb-4`}>
-        Note Medication Progress
-      </Text>
-      <Text
-        style={tw`w-48 text-center pb-5 text-sm`}>
-        Always record your medication progress for your future reference.
-      </Text>
-      <Button
-        title="Next"
-        color={'#F13A3B'}
-        onPress={() => navigation.navigate('OnboardingScreen2')}
-      />
-    </View>
+    <SafeAreaView style={tw``}>
+      <View style={tw`items-center justify-start h-full mx-3  w-full`}>
+        <View>
+          <Image
+            source={{
+              uri: 'https://rebrand.ly/4444772',
+            }}
+            style={tw`relative h-96 w-96 mb-24 bg-transparent border-transparent`}
+          />
+          <Image
+            source={{
+              uri: 'https://rebrand.ly/daff5b',
+            }}
+            style={tw`absolute h-[216px] inset-24 border-transparent w-[173px] mb-24 bg-transparent border-transparent`}
+          />
+        </View>
+
+        <Text style={tw`text-3xl font-bold text-black text-center mr-5 pb-4`}>
+          Note Medication Progress
+        </Text>
+        <Text style={tw`w-48 text-center pb-5 text-sm mr-5`}>
+          Always record your medication progress for your future reference.
+        </Text>
+
+        <View style={tw`w-84 p-5 mr-6`}>
+          <Button
+            title="Next"
+            color={'#F13A3B'}
+            onPress={() => navigation.navigate('OnboardingScreen2')}
+          />
+          <Text style={tw`mt-3 text-center text-lg text-[#F13A3B]`}>Skip</Text>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 };
 
