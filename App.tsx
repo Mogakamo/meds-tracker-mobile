0bbcad793c285bb6c559e23ba15f5a8504f24118
Mogakamo <mogaka.amo254@gmail.com>
2022-08-24 15:06:20 +0300

    build: Added the screens

--- App.tsx
+++ App.tsx
@@ -1,41 +1,12 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
 import React, {type PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+
+// tailwindcss 
+import tw from 'twrnc'
 
 const App = () => {
   return (
-    <SafeAreaView>
-      <ScrollView contentInsetAdjustmentBehavior="automatic">
-        <View>
-          <Text>Hello world</Text>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+   <></>
   );
 };
 
