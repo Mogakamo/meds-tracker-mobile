b7c049d97711d5bf27421a06d8d4baafd21fbee4
Mogakamo <mogaka.amo254@gmail.com>
2022-08-24 17:06:34 +0300

    build: onboarding screens are done

--- App.tsx
+++ App.tsx
@@ -7,6 +7,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Homepage from './src/screens/Homepage';
 import OnboardingScreen1 from './src/screens/onboarding/OnboardingScreen1';
 import OnboardingScreen2 from './src/screens/onboarding/OnboardingScreen2';
+import OnboardingScreen3 from './src/screens/onboarding/OnboardingScreen3';
 
 const AppStack = createNativeStackNavigator()
 
@@ -16,7 +17,8 @@ const App = () => {
     <AppStack.Navigator>
       {/* <AppStack.Screen name="Homepage" component={Homepage} /> */}
       {/* <AppStack.Screen name="OnboardingScreen 1" component={OnboardingScreen1} /> */}
-      <AppStack.Screen name="OnboardingScreen 2" component={OnboardingScreen2} />
+      {/* <AppStack.Screen name="OnboardingScreen 2" component={OnboardingScreen2} /> */}
+      <AppStack.Screen name="OnboardingScreen 3" component={OnboardingScreen3} />
     </AppStack.Navigator>
    </NavigationContainer>
   );
--- src/screens/onboarding/OnboardingScreen3.tsx
+++ src/screens/onboarding/OnboardingScreen3.tsx
@@ -1,12 +1,47 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {View, Text, Button, Linking, Image} from 'react-native';
+import React, {useLayoutEffect} from 'react';
+import tw from '../../lib/tailwind';
+import {useNavigation} from '@react-navigation/native';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import classNames from 'classnames';
 
-const OnboardingScreen3 = () => {
+// type Props = NativeStackScreenProps<>;
+type Props = {
+  route: any;
+  navigation: any;
+};
+
+const OnboardingScreen3 = ({route, navigation}: Props) => {
+  const navigate = useNavigation();
+
+  useLayoutEffect(() => {
+    navigate.setOptions({
+      headerShown: false,
+    });
+  }, []);
   return (
-    <View>
-      <Text>OnboardingScreen3</Text>
+    <View style={tw`items-center justify-center h-full mx-3`}>
+      <Image
+        source={{
+          uri: 'https://rebrand.ly/23c9e5',
+        }}
+        resizeMode="contain"
+        style={tw`h-[172px] w-[240px] mb-24 bg-transparent border-transparent`}
+      />
+      <Text style={tw`text-3xl font-bold text-black pb-4`}>
+        Dates Are Always Important
+      </Text>
+      <Text
+        style={tw`w-48 text-center pb-5 text-sm`}>
+        The calendar never forgets. Keep track of your medication dates at all times.
+      </Text>
+      <Button
+        title="Next"
+        color={'#F13A3B'}
+        onPress={() => navigation.navigate('OnboardingScreen2')}
+      />
     </View>
-  )
-}
+  );
+};
 
-export default OnboardingScreen3
+export default OnboardingScreen3;
